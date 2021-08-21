1da8cd30677ccd47db565f49c3d800ac3dd07ec5
Hans-Eric Lippke <hans_eric_lippke@hotmail.com>
2021-08-21 00:04:03 -0400

    Updated mobile design to be able to display

--- src/Nav.js
+++ src/Nav.js
@@ -4,23 +4,23 @@ import me from './me.jpg';
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="flex items-center h-screen bg-mainbg bg-cover">
+    <div className="md:flex items-center md:h-screen v-screen bg-blurp md:bg-mainbg md:bg-cover">
       <div className="container mx-auto max-w-5xl">
-        <div className="bg-white rounded-lg shadow-lg p-5 md:p-10 mx-2 border-mainblue-dark border-8">
+        <div className="bg-mainblue-darkest md:bg-white rounded-lg shadow-lg p-5 md:p-10 md:mt-20 mx-2 border-none md:border-mainblue-dark md:border-8">
           <div className="text-center">
-            <h2 className="text-3xl tracking-tight leading-10 font-extrabold text-mainblue sm:text-5xl sm:leading-none md:text-6xl">
-              Welcome to <span className="text-blurp">Hans-Eric Lippke's</span>
+            <h2 className="text-2xl tracking-tight leading-none font-extrabold text-mainblue md:leading-none md:text-5xl">
+              Welcome to <span className="md:text-blurp text-white">Hans-Eric Lippke's</span>
             </h2>
-            <h2 className="text-4xl tracking-tight leading-10 mt-9 font-extrabold text-black sm:text-5xl sm:leading-none md:text-6xl">
-                <span className="text-pinky">Portfolio/Resume Site</span>
+            <h2 className="text-2xl tracking-tight  break-all  mt-9 font-extrabold text-pinky leading-none md:leading-10 md:text-5xl">
+              Portfolio/Resume Site
             </h2>
-            <h2 className="text-2xl tracking-tight leading-10 mt-9 font-extrabold text-bled sm:text-5xl sm:leading-none md:text-6xl">
-                      Coming Soon...ish
+            <h2 className="text-2xl tracking-tight   mt-9 font-extrabold text-bled leading-none md:leading-10 md:text-5xl ">
+              Coming Soon...ish
             </h2>
             <h3 className='text-md md:text-2xl text-center mt-4 font-bold text-bled'>Busy learning/playing with react and tailwind</h3>
           </div>
    
-          <div className="max-w-screen-md py-4 px-8 bg-mainblue-darkest shadow-lg rounded-lg mt-20 m-auto">
+          <div className="max-w-screen-md py-4 px-8 bg-mainblue-dark md:bg-mainblue-darkest shadow-lg rounded-lg mt-20 m-auto">
             <div className="flex justify-center md:justify-end -mt-16">
               <img className="w-24 h-24 object-cover rounded-lg border-2 border-mainblue-dark"  src={me} alt="Picture of me" />
             </div>
